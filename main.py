fba38a0483939054970395c2f6dd70e51d3f5f75
sukonnbu <choijiwan1206@gmail.com>
2023-11-04 01:29:49 +0900

    add equation-solving function

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def get_square_gravity(arr: np.ndarray) :
     x_1, y_1 = get_triangle_gravity()
     x_2, y_2 = get_triangle_gravity()
     # 두 점 지나는 직선(일차방정식)
-    solution_1 = np.array([y_2 -y_1, -x_2 +x_1, x_2*y_1 -y_2*x_1])
+    solution_1 = np.array([y_2 -y_1, -x_2 +x_1, -x_2*y_1 +y_2*x_1])
     print(solution_1)
 
     return np.array([x, y])
@@ -31,3 +31,15 @@ def get_distance(arr: np.ndarray) :
         dist_array = np.append(dist_array, np.sum((nth_arr - arr[0])**2))
 
     return dist_array
+
+'''
+일차방정식의 근 찾기
+[   [a b c]             ax + by = c
+    [d e f]    ] =>     dx + ey = f
+'''
+# 값 0일 때 예외처리 필요
+def solve_equation(sol_1: np.ndarray, sol_2: np.ndarray):
+    sol_1 -= sol_2 *sol_1[0]/sol_2[0]
+    sol_2 -= sol_1 *sol_2[1]/sol_1[1]
+
+    return(sol_2[2]/sol_2[0], sol_1[2]/sol_1[1])

e4c237b7b958b2a555fee2064dedc406e6dca479
sukonnbu <choijiwan1206@gmail.com>
2023-11-04 23:28:10 +0900

    polygon gravity algorythm

--- find_point.py
+++ find_point.py
@@ -1,11 +1,12 @@
 import numpy as np
-import decimal
 import math
 
 
 # 삼각형의 무게중심
 def get_triangle_gravity(arr: np.ndarray):
-    return np.array([np.sum(arr[:0])/3, np.sum(arr[:1])/3])
+    point = np.array([np.sum(arr[:,0])/3, np.sum(arr[:,1])/3])
+    print("Point:", np.array([np.sum(arr[:,0])/3, np.sum(arr[:,1])/3]))
+    return point
 
 # 사각형의 무게중심
 # 점선택 알고리즘 -> 무게중심 두 번 구함
@@ -15,6 +16,7 @@ def get_square_gravity(square_arr: np.ndarray):
 
     x_1, y_1 = get_triangle_gravity(np.delete(points_arr, 3, axis=0))
     x_2, y_2 = get_triangle_gravity(np.delete(points_arr, 1, axis=0))
+    print(np.array([[x_1, y_1], [x_2, y_2]]))
     # 두 점 지나는 직선(일차방정식)
     solution_1 = np.array([y_2 - y_1, x_1 - x_2, y_2 * x_1 - x_2 * y_1])
     print(f"Sol1: {solution_1[0]} X {solution_1[1]} Y = {solution_1[2]}")
@@ -24,9 +26,28 @@ def get_square_gravity(square_arr: np.ndarray):
     x_4, y_4 = get_triangle_gravity(np.delete(points_arr, 0, axis=0))
 
     solution_2 = np.array([y_4 - y_3, x_3 - x_4, x_3 * y_4 - x_4 * y_3])
-    print(f"Sol1: {solution_2[0]} X {solution_2[1]} Y = {solution_2[2]}")
+    print(f"Sol2: {solution_2[0]} X {solution_2[1]} Y = {solution_2[2]}")
 
     return solve_equation(solution_1, solution_2)
+
+
+def get_polygon_gravity(nth_arr: np.ndarray):
+    if nth_arr.shape[0] == 3:
+        return get_triangle_gravity(nth_arr)
+    else:
+        points_arr = sort_unclockwise(nth_arr)
+
+        x_1, y_1 = get_triangle_gravity(points_arr[0:3])
+        x_2, y_2 = get_polygon_gravity(np.delete(points_arr, 1, axis=0))
+        # 두 점 지나는 직선(일차방정식)
+        solution_1 = np.array([y_2 - y_1, x_1 - x_2, y_2 * x_1 - x_2 * y_1])
+
+        x_3, y_3 = get_triangle_gravity(points_arr[-3:])
+        x_4, y_4 = get_polygon_gravity(np.delete(points_arr, -2, axis=0))
+
+        solution_2 = np.array([y_4 - y_3, x_3 - x_4, x_3 * y_4 - x_4 * y_3])
+
+        return solve_equation(solution_1, solution_2)
 
 
 #반시계방향으로 정렬 -> 삼각 분할 위해
@@ -51,20 +72,6 @@ def sort_unclockwise(arr: np.ndarray):
     return sorted_arr.reshape((int(arr.size/2), 2))
 
 
-# 안 써도 돌아갈 거 같음
-'''
-# 거리^2 행렬
-# 행렬의 첫 번째 점을 기준으로 거리 구함
-def get_distance(arr: np.ndarray):
-    dist_array = np.array([])
-    for nth_arr in arr:
-        # (x-x_n)^2 + (y-y_n)^2
-        dist_array = np.append(dist_array, np.sum((nth_arr - arr[0])**2))
-
-    print("Distance:", dist_array[1:])
-    return dist_array[1:]
-'''
-
 '''
 일차방정식의 근 리턴함
 [   [a b c]             ax + by = c
@@ -72,11 +79,10 @@ def get_distance(arr: np.ndarray):
 '''
 # 값 0일 때 예외처리 필요
 def solve_equation(sol_1: np.ndarray, sol_2: np.ndarray):
-    sol_1 -= sol_2 * (sol_1[0] / sol_2[0])
-    sol_2 -= sol_1 * (sol_2[1] / sol_1[1])
+    if sol_2[0] != 0:
+        sol_1 = sol_1 - sol_2 * (sol_1[0] / sol_2[0])
+
+    if sol_1[1] != 0:
+        sol_2 = sol_2 - sol_1 * (sol_2[1] / sol_1[1])
 
     return np.array([sol_2[2] / sol_2[0], sol_1[2] / sol_1[1]])
-
-
-if __name__ == "__main__":
-    pass
--- show_graph.py
+++ show_graph.py
@@ -1,12 +1,12 @@
+import random
 import matplotlib.pyplot as plt
 import numpy as np
 import find_point
 
 #점 4개 찍으면 무게중심 찾아줌
-dots = np.array([
-])
+dots = np.random.rand(5, 2)
 
-gravity_point = find_point.get_square_gravity(dots)
+gravity_point = find_point.get_polygon_gravity(dots)
 plt.figure()
 for dot in dots:
     plt.plot(dot[0], dot[1], "bo-")
